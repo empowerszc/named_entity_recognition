0585968221ad5183a844b64d7fda48b85e70a80a
Zhu Chuan <39799350+empowerszc@users.noreply.github.com>
2021-07-15 16:53:49 +0800

    重构原作者的代码，并加上了预测函数。

--- main.py
+++ main.py
@@ -1,73 +1,127 @@
+import sys
+import time
 
-from data import build_corpus
-from utils import extend_maps, prepocess_data_for_lstmcrf
-from evaluate import hmm_train_eval, crf_train_eval, \
-    bilstm_train_and_eval, ensemble_evaluate
+
+import argparse
+from utils.data_util import build_corpus, build_map ,save_params,load_params
+from utils.preprocess_util import save_model,load_model,flatten_lists,extend_maps
+from utils.evaluating import Metrics
+from utils.config import MODEL_LIST
+from models.model_factory import getModel
+
+parser = argparse.ArgumentParser(description="Named Entity Recognition")
+parser.add_argument("action", type=str, help="train | test | predict")
+parser.add_argument("--model", type=str, default="BILSTM_CRF")
+parser.add_argument("--dataset", type=str, default="ResumeNER")
+parser.add_argument("--text", type=str, )
+args = parser.parse_args()
 
 
 def main():
-    """训练模型，评估结果"""
+    # 没有多的其他参数，读取配置文件
+    # if len(sys.argv) ==1:
+    model_name = args.model
 
-    # 读取数据
-    print("读取数据...")
-    train_word_lists, train_tag_lists, word2id, tag2id = \
-        build_corpus("train")
-    dev_word_lists, dev_tag_lists = build_corpus("dev", make_vocab=False)
-    test_word_lists, test_tag_lists = build_corpus("test", make_vocab=False)
+    assert  model_name in MODEL_LIST, "模型不存在"
 
-    # 训练评估ｈｍｍ模型
-    print("正在训练评估HMM模型...")
-    hmm_pred = hmm_train_eval(
-        (train_word_lists, train_tag_lists),
-        (test_word_lists, test_tag_lists),
-        word2id,
-        tag2id
-    )
+    if model_name == "BILSTM_CRF":
+        # model_name == "BILSTM"
+        for_crf = True
+    else:
+        for_crf = False
 
-    # 训练评估CRF模型
-    print("正在训练评估CRF模型...")
-    crf_pred = crf_train_eval(
-        (train_word_lists, train_tag_lists),
-        (test_word_lists, test_tag_lists)
-    )
+    eval = True
 
-    # 训练评估BI-LSTM模型
-    print("正在训练评估双向LSTM模型...")
-    # LSTM模型训练的时候需要在word2id和tag2id加入PAD和UNK
-    bilstm_word2id, bilstm_tag2id = extend_maps(word2id, tag2id, for_crf=False)
-    lstm_pred = bilstm_train_and_eval(
-        (train_word_lists, train_tag_lists),
-        (dev_word_lists, dev_tag_lists),
-        (test_word_lists, test_tag_lists),
-        bilstm_word2id, bilstm_tag2id,
-        crf=False
-    )
+    # 准备数据集
+    dataset = args.dataset
+    data_dir = "./datasets/"+dataset +"/data/"
+    train_word_lists, train_tag_lists = build_corpus("train", data_dir=data_dir)
+    dev_word_lists, dev_tag_lists = build_corpus("dev", data_dir=data_dir)
+    test_word_lists, test_tag_lists = build_corpus("test", data_dir=data_dir)
 
-    print("正在训练评估Bi-LSTM+CRF模型...")
-    # 如果是加了CRF的lstm还要加入<start>和<end> (解码的时候需要用到)
-    crf_word2id, crf_tag2id = extend_maps(word2id, tag2id, for_crf=True)
-    # 还需要额外的一些数据处理
-    train_word_lists, train_tag_lists = prepocess_data_for_lstmcrf(
-        train_word_lists, train_tag_lists
-    )
-    dev_word_lists, dev_tag_lists = prepocess_data_for_lstmcrf(
-        dev_word_lists, dev_tag_lists
-    )
-    test_word_lists, test_tag_lists = prepocess_data_for_lstmcrf(
-        test_word_lists, test_tag_lists, test=True
-    )
-    lstmcrf_pred = bilstm_train_and_eval(
-        (train_word_lists, train_tag_lists),
-        (dev_word_lists, dev_tag_lists),
-        (test_word_lists, test_tag_lists),
-        crf_word2id, crf_tag2id
-    )
+    data = [(train_word_lists,train_tag_lists), (dev_word_lists, dev_tag_lists), (test_word_lists, test_tag_lists)]
 
-    ensemble_evaluate(
-        [hmm_pred, crf_pred, lstm_pred, lstmcrf_pred],
-        test_tag_lists
-    )
+    # 字典数据持久化
+    word2id = build_map(train_word_lists)
+    tag2id = build_map(train_tag_lists)
+    word_tag_map = {
+        "word2id":word2id,
+        "tag2id":tag2id,
+    }
+    # 将数据存储起来
+    save_params(data_dir, word_tag_map)
+
+    print("正在训练评估{}模型...".format(model_name))
+    train_eval(model_name, dataset, data, word_tag_map, for_crf, eval=eval)
+
+
+def train_eval(model_name,dataset, data, word_tag_map, for_crf, eval=True):
+    # 一般都是3个
+    if len(data) == 3:
+        train_data = data[0]
+        dev_data = data[1]
+        test_data = data[2]
+    elif len(data) ==2:
+        train_data = data[0]
+        dev_data = None
+        test_data = data[1]
+
+
+    model = getModel(model_name, word_tag_map, for_crf)
+    saved_path = "./datasets/"+dataset + "/ckpts/" + model_name.lower()+".pkl"
+
+    start = time.time()
+    model.train(train_data, dev_data, word_tag_map, for_crf)
+
+    print("训练完毕,共用时{}秒.".format(int(time.time() - start)))
+
+    save_model(model, saved_path)
+
+    if eval == True:
+        print("评估模型{}".format(model_name))
+
+        pred, target_tag_list = model.test(test_data, word_tag_map, for_crf)
+
+        metrics = Metrics(target_tag_list, pred, remove_O=False) # 在评估的时候是否去除O标记
+        metrics.report_scores()
+        metrics.report_confusion_matrix()
+
+
+def test():
+    model_name = args.model
+    dataset = args.dataset
+    data_dir = "./datasets/"+dataset +"/data/"
+    model_path = "./datasets/"+dataset + "/ckpts/" + model_name.lower()+".pkl"
+
+    word_tag_map = load_params(data_dir)
+    word2id = word_tag_map["word2id"]
+    tag2id = word_tag_map["tag2id"]
+
+    if "BILSTM" in model_name:
+        if model_name == "BILSTM_CRF":
+            for_crf = True
+        else:
+            for_crf = False
+        word2id, tag2id = extend_maps(word2id, tag2id, for_crf=for_crf)
+
+    test_word_lists, test_tag_lists = build_corpus("test", data_dir=data_dir)
+    test_data = (test_word_lists, test_tag_lists)
+
+
+
+    print("评估{}模型".format(model_name))
+    print(model_path)
+    model = load_model(model_path)
+
+    pred, target_tag_list = model.test(test_data, word_tag_map,for_crf)
+
+    metrics = Metrics(target_tag_list, pred, remove_O=False)  # 在评估的时候是否去除O标记
+    metrics.report_scores()
+    metrics.report_confusion_matrix()
 
 
 if __name__ == "__main__":
-    main()
+    if args.action == "train":
+        main()
+    elif args.action == "test":
+        test()
